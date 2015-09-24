927ee1ae3f92c2e501ad7c6a357632a497b92b9f
MedeirosMarcelo <marcelomedeiros-@hotmail.com>
2015-09-24 16:46:19 -0300

    Start

---
+++ Server/Assets/Meu/CommandReader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandReader : MonoBehaviour {
+
+    public void ReadScript(string str) {
+
+        CheckCommand(str);
+    }
+
+    string GetCommand(string str) {
+        return "";
+    }
+
+    void CheckCommand(string str) {
+
+        string command = GetCommand(str);
+        Debug.Log("@" + command + ": ");
+
+        switch (command) {
+            case "wait":
+                break;
+            default:
+                Debug.Log("WRONG COMMAND: " + command);
+                break;
+        }
+    }
+}
